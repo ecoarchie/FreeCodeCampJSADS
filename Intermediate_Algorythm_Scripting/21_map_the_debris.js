efd2516e9d6dbead7de3d27e5b70e778d77e71e7
ecoarchie <eco.artur@gmail.com>
2022-11-07 05:18:00 +0300

    minor format changes

--- Intermediate_Algorythm_Scripting/21_map_the_debris.js
+++ Intermediate_Algorythm_Scripting/21_map_the_debris.js
@@ -2,7 +2,18 @@ function orbitalPeriod(arr) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
 
-  return arr.map((el) =>({name: el.name, orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + el.avgAlt, 3) / GM))}));
+  return arr.map((el) => ({
+    name: el.name,
+    orbitalPeriod: Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(earthRadius + el.avgAlt, 3) / GM)
+    ),
+  }));
 }
 
-console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
+console.log(
+  orbitalPeriod([
+    { name: "iss", avgAlt: 413.6 },
+    { name: "hubble", avgAlt: 556.7 },
+    { name: "moon", avgAlt: 378632.553 },
+  ])
+);
